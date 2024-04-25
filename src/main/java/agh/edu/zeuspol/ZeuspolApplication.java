53b539bd6ef49beee0d31511d9046f47a48519b9
Pepesob <pepesob2@gmail.com>
2024-04-25 14:12:59 +0200

    Initial commit

---
+++ src/main/java/agh/edu/zeuspol/ZeuspolApplication.java
@@ -0,0 +1,13 @@
+package agh.edu.zeuspol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeuspolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeuspolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/agh/edu/zeuspol/ZeuspolApplicationTests.java
@@ -0,0 +1,13 @@
+package agh.edu.zeuspol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeuspolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
