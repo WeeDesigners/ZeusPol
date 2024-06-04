79bb62d9633af769b25e287a50d7c5c9d88d5cb1
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2024-06-04 23:10:03 +0200

    Parsing JSON done (not tested)

--- src/main/java/agh/edu/zeuspol/data_structures/NotificationRule.java
+++ src/main/java/agh/edu/zeuspol/data_structures/NotificationRule.java
@@ -2,11 +2,11 @@ package agh.edu.zeuspol.data_structures;
 
 import java.util.List;
 
-public class Notification extends Rule {
+public class NotificationRule extends Rule {
 
     public final String email;
 
-    public Notification(RuleAttribute attribute, RuleSubject subject, List<Number> value, UnitType unit, ActionType action, String email) {
+    public NotificationRule(RuleAttribute attribute, RuleSubject subject, List<Number> value, UnitType unit, ActionType action, String email) {
         super(attribute, subject, value, unit, action);
         this.email = email;
     }
--- src/main/java/agh/edu/zeuspol/parsers/RuleJsonParser.java
+++ src/main/java/agh/edu/zeuspol/parsers/RuleJsonParser.java
@@ -1,18 +1,55 @@
 package agh.edu.zeuspol.parsers;
 
+
+import agh.edu.zeuspol.data_structures.NotificationRule;
 import agh.edu.zeuspol.data_structures.Rule;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import java.util.List;
 
 public class RuleJsonParser {
 
+    private final String jsonString;
+    private List<Rule> rules;
+    private List<NotificationRule> notificationRules;
 
-
-    public RuleJsonParser(){
-
+    public RuleJsonParser(String jsonString){
+        this.jsonString = jsonString;
     }
 
-    private Rule parseRuleString(String ruleString){
-        return null;
+    public void parseJson(){
+
+        Gson gson = new Gson();
+
+
+        JsonObject[] jsonObjects = gson.fromJson(jsonString, JsonObject[].class);
+
+        for(JsonObject jsonObject : jsonObjects){
+            if(jsonObject.has("email")){
+                notificationRules.add(gson.fromJson(jsonObject, NotificationRule.class));
+            }
+            else{
+                rules.add(gson.fromJson(jsonObject, Rule.class));
+            }
+        }
     }
+
+    public List<Rule> getRules() {
+        return rules;
+    }
+
+    public List<NotificationRule> getNotificationRules() {
+        return notificationRules;
+    }
+
+
+
+
+
+
+
+
 
 
 }
