bdf42806ee848649de9db9866823378e484ee179
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2024-06-04 22:38:38 +0200

    Initialize parser class implementation

---
+++ src/main/java/agh/edu/zeuspol/parsers/RuleJsonParser.java
@@ -0,0 +1,18 @@
+package agh.edu.zeuspol.parsers;
+
+import agh.edu.zeuspol.data_structures.Rule;
+
+public class RuleJsonParser {
+
+
+
+    public RuleJsonParser(){
+
+    }
+
+    private Rule parseRuleString(String ruleString){
+        return null;
+    }
+
+
+}
