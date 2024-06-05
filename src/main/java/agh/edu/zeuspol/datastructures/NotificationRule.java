aa3a3eaea496112530ab3312246b09f0e8aab9a9
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2024-06-05 22:31:48 +0200

    Generated equals() and hashCode() methods for Rule and NotificationRule classes

--- src/main/java/agh/edu/zeuspol/datastructures/NotificationRule.java
+++ src/main/java/agh/edu/zeuspol/datastructures/NotificationRule.java
@@ -1,6 +1,7 @@
 package agh.edu.zeuspol.datastructures;
 
 import java.util.List;
+import java.util.Objects;
 
 public class NotificationRule extends Rule {
 
@@ -9,6 +10,20 @@ public class NotificationRule extends Rule {
     public NotificationRule(RuleAttribute attribute, RuleSubject subject, List<Number> value, UnitType unit, ActionType action, String email) {
         super(attribute, subject, value, unit, action);
         this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        NotificationRule that = (NotificationRule) o;
+        return Objects.equals(email, that.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), email);
     }
 
     @Override
--- src/main/java/agh/edu/zeuspol/datastructures/Rule.java
+++ src/main/java/agh/edu/zeuspol/datastructures/Rule.java
@@ -3,6 +3,7 @@ package agh.edu.zeuspol.datastructures;
 import agh.edu.zeuspol.generators.IdGenerator;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Rule {
     public final long id;
@@ -22,8 +23,24 @@ public class Rule {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Rule rule = (Rule) o;
+        return attribute == rule.attribute && subject == rule.subject && Objects.equals(value, rule.value) && unit == rule.unit && action == rule.action;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(attribute, subject, value, unit, action);
+    }
+
+    @Override
     public String toString() {
         //TODO -> better concat
         return "{ " + id + ", " + attribute + ", " + subject + ", " + value + ", " + unit + ", " + action + " }";
     }
+
 }
