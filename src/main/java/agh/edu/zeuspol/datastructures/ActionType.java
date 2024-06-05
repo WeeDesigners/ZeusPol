8953415b37df13ffffc698fab17515831dd3a8bc
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2024-06-05 23:03:59 +0200

    Added function definitions to ActinType enum

--- src/main/java/agh/edu/zeuspol/datastructures/ActionType.java
+++ src/main/java/agh/edu/zeuspol/datastructures/ActionType.java
@@ -1,9 +1,46 @@
 package agh.edu.zeuspol.datastructures;
 
+import java.util.List;
+
 public enum ActionType {
-    GT,
-    LT,
-    EQ,
-    BT,
+
+    //to execute functions use:
+    // ActionType.TYPE.apply(arg1, arg2);
+
+    GT {
+        @Override
+        public boolean apply(List<Number> list, Number n2){
+            Number n1 = list.get(0);
+            return (n1.doubleValue() > n2.doubleValue());
+        }
+    },
+    LT{
+        @Override
+        public boolean apply(List<Number> list, Number n2){
+            Number n1 = list.get(0);
+            return (n1.doubleValue() < n2.doubleValue());
+        }
+    },
+    EQ{
+        @Override
+        public boolean apply(List<Number> list, Number n2){
+            Number n1 = list.get(0);
+            return n1.doubleValue() == n2.doubleValue();
+        }
+    },
+    BT{
+        @Override
+        public boolean apply(List<Number> list, Number n2){
+            Number a = list.get(0);
+            Number b = list.get(1);
+            return (n2.doubleValue() > a.doubleValue()) && (n2.doubleValue() < b.doubleValue());
+        }
+    },
+    ;
     //etc.
+
+
+
+
+    public abstract boolean apply(List<Number> list, Number n2);
 }
