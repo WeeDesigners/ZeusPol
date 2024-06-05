38ca1576514f2d732482f42124a3f18fa12a472c
Pepesob <pepesob2@gmail.com>
2024-06-06 01:28:24 +0200

    Implemented SlaViolationChecker

--- src/main/java/agh/edu/zeuspol/checker/SlaViolationChecker.java
+++ src/main/java/agh/edu/zeuspol/checker/SlaViolationChecker.java
@@ -14,44 +14,62 @@ public class SlaViolationChecker {
                 if (slaRule.unit != rule.unit){
                     throw new IllegalArgumentException();
                 }
-                if (slaRule.action == ActionType.BT && rule.action == ActionType.BT){
-                    if (this.isBetween(slaRule.value, rule.value)){
-                        return false;
+                if (slaRule.action == ActionType.EQ || rule.action == ActionType.EQ){
+                    if (slaRule.action == ActionType.EQ && rule.action == ActionType.EQ){
+                        ActionType action = slaRule.action;
+                        if (!action.apply(slaRule.value, rule.value.get(0))){
+                            return true;
+                        }
+                    }
+                    else {
+                        return true;
+                    }
+                }
+                else if (slaRule.action == ActionType.BT && rule.action == ActionType.BT){
+                    if (!this.isBetween(slaRule.value, rule.value)){
+                        return true;
                     }
                 }
                 else if (slaRule.action == ActionType.BT){
-                    if (!(rule.action.apply(rule.value, slaRule.value.get(0)) && rule.action.apply(rule.value, slaRule.value.get(1)))){
-                        return false;
+                    ActionType action = rule.action;
+                    Number n1 = slaRule.value.get(0);
+                    Number n2 = slaRule.value.get(1);
+                    if (!(action.apply(rule.value, n1) || action.apply(rule.value, n2))){
+                        return true;
                     }
                 }
                 else if (rule.action == ActionType.BT){
-
+                    ActionType action = slaRule.action;
+                    Number n1 = rule.value.get(0);
+                    Number n2 = rule.value.get(1);
+                    if (!(action.apply(slaRule.value, n1) && action.apply(slaRule.value, n2))){
+                        return true;
+                    }
                 }
                 else if (slaRule.action != rule.action){
-                    return false;
+                    return true;
                 }
                 else {
-                    if (handleSameAction(slaRule, rule)){
-                        return false;
+                    ActionType action = slaRule.action;
+                    if (action.apply(slaRule.value, rule.value.get(0))){
+                        return true;
                     }
                 }
             }
         }
-        return true;
+        return false;
     }
 
     private boolean isBetween(List<Number> span1, List<Number> span2){
+        if (span1.size() != 2 || span2.size() != 2){
+            throw new RuntimeException("Wrong argument number in lists!");
+        }
         double n1 = span1.get(0).doubleValue();
         double n2 = span1.get(1).doubleValue();
 
         double m1 = span2.get(0).doubleValue();
         double m2 = span2.get(1).doubleValue();
         return n1 <= m1 && n1 <= m2 && n2 >= m1 && n2 >= m2;
-    }
-
-    private boolean handleSameAction(Rule slaRule, Rule rule) {
-        ActionType action = slaRule.action;
-        return action.apply(slaRule.value, rule.value.get(0));
     }
 
 }
