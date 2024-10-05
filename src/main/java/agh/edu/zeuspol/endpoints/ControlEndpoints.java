89fd5deff73bd185a92327509da44a2be9ab61e0
socz3qqq <soczawa@student.agh.edu.pl>
2024-10-05 19:36:14 +0200

    feat: added connection to themis service
    [PC-27]

--- src/main/java/agh/edu/zeuspol/endpoints/ControlEndpoints.java
+++ src/main/java/agh/edu/zeuspol/endpoints/ControlEndpoints.java
@@ -1,35 +1,56 @@
 package agh.edu.zeuspol.endpoints;
 
+
 import agh.edu.zeuspol.ZeuspolApplication;
+import agh.edu.zeuspol.endpoints.requests.ExecutionRequest;
+import agh.edu.zeuspol.services.ThemisService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/app")
 public class ControlEndpoints {
 
-  @PostMapping("/start")
-  public String startApp() {
-    if (ZeuspolApplication.isRunning()) {
-      return "App is already running";
+    private final ThemisService themisService;
+
+    @Autowired
+    public ControlEndpoints(ThemisService themisService) {
+        this.themisService = themisService;
     }
 
-    // start app
-    ZeuspolApplication.startApp();
+    @PostMapping("/start")
+    public String startApp() {
+        if(ZeuspolApplication.isRunning()){
+            return "App is already running";
+        }
 
-    return "ZeusPol started!";
-  }
+        //start app
+        ZeuspolApplication.startApp();
 
-  @PostMapping("/stop")
-  public String stopApp() {
-    if (!ZeuspolApplication.isRunning()) {
-      return "App is already not running";
+        return "ZeusPol started!";
     }
 
-    // stop app
-    ZeuspolApplication.stopApp();
 
-    return "ZeusPol stopped!";
-  }
+    @PostMapping("/stop")
+    public String stopApp() {
+        if(!ZeuspolApplication.isRunning()){
+            return "App is already not running";
+        }
+
+        //stop app
+        ZeuspolApplication.stopApp();
+
+        return "ZeusPol stopped!";
+    }
+
+    @PostMapping("/execute-action")
+    public String executeThemis(@RequestBody ExecutionRequest request) {
+        String response = this.themisService.executeAction(request);
+        return "Themis response: " + response;
+    }
+
 }
