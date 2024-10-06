e2c93eb1796e83b9c23a725a6a5ea9ad34fbb9c7
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2024-10-06 20:42:52 +0200

    Revert "Hermes connection (#25)"
    This reverts commit b9974821c41af1f6849508023102c89c2ff06fa2.

--- src/main/java/agh/edu/zeuspol/endpoints/PoliciesEndpoints.java
+++ src/main/java/agh/edu/zeuspol/endpoints/PoliciesEndpoints.java
@@ -3,21 +3,12 @@ package agh.edu.zeuspol.endpoints;
 import agh.edu.zeuspol.datastructures.Rule;
 import agh.edu.zeuspol.datastructures.storage.Policies;
 import agh.edu.zeuspol.parsers.RuleJsonParser;
-import agh.edu.zeuspol.services.HermesService;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/policies")
 public class PoliciesEndpoints {
-
-  private final HermesService hermesService;
-
-  @Autowired
-  public PoliciesEndpoints(HermesService hermesService) {
-    this.hermesService = hermesService;
-  }
 
   @PostMapping("/addPolicies")
   public String addPolicies(@RequestBody String policiesString) {
@@ -37,37 +28,6 @@ public class PoliciesEndpoints {
       return "Policies updated successfully!";
     }
     return "Policies updated unsuccessfully! One or more rules are not violent with SLA!";
-  }
-
-  @PostMapping("/exportAll")
-  public String exportAllToHermes() {
-    Policies policies = Policies.getInstance();
-    List<Rule> rules = policies.getRules();
-
-    String response = "";
-
-    for (Rule rule : rules) {
-      String hermesResponse = hermesService.addRuleObject(rule);
-      // these '+=' are really cute c:
-      if (hermesResponse != null && hermesResponse.equals("Rule added successfully")) {
-        response += "Rule id=" + rule.id + " exported succesfully!\n";
-      } else {
-        response += "Rule id=" + rule.id + " not exported\n";
-      }
-    }
-    return response;
-  }
-
-  @PostMapping("/importRule")
-  public String importRuleFromHermes(@RequestBody long id) {
-    Policies policies = Policies.getInstance();
-
-    Rule rule = hermesService.getRuleObject(id);
-    if (rule != null) {
-      policies.addRule(rule);
-      return "Successfully imported rule: " + rule;
-    }
-    return "Error occurred while importing rule id=" + id;
   }
 
   @GetMapping("getPolicies")
