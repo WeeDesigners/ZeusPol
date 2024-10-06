4e563e962160d918098e2d8ef77da70c5bf84610
EfJot314 <EfJot314@users.noreply.github.com>
2024-10-06 17:28:29 +0000

    chore: fix linting issues

--- src/main/java/agh/edu/zeuspol/services/HermesService.java
+++ src/main/java/agh/edu/zeuspol/services/HermesService.java
@@ -10,60 +10,59 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-
 @Service
 public class HermesService {
-    private final RestTemplate restTemplate;
+  private final RestTemplate restTemplate;
 
-    @Value("${hermes.url}")
-    private String hermesUrl = "http://127.0.0.2:8080";
+  @Value("${hermes.url}")
+  private String hermesUrl = "http://127.0.0.2:8080";
 
-    public HermesService(RestTemplateBuilder restTemplateBuilder) {
-        this.restTemplate = restTemplateBuilder.build();
+  public HermesService(RestTemplateBuilder restTemplateBuilder) {
+    this.restTemplate = restTemplateBuilder.build();
+  }
+
+  public String addRuleObject(Rule rule) {
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+
+    HttpEntity<Rule> requestEntity = new HttpEntity<>(rule, headers);
+
+    try {
+      return this.restTemplate.postForObject(
+          hermesUrl + "/rules/addRuleObject", requestEntity, String.class);
+    } catch (HttpClientErrorException e) {
+      return e.getResponseBodyAsString();
     }
+  }
 
-    public String addRuleObject(Rule rule) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
+  public String addRuleString(String ruleString) {
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
 
-        HttpEntity<Rule> requestEntity = new HttpEntity<>(rule, headers);
+    HttpEntity<String> requestEntity = new HttpEntity<>(ruleString, headers);
 
-        try {
-            return this.restTemplate.postForObject(hermesUrl + "/rules/addRuleObject", requestEntity, String.class);
-        } catch (HttpClientErrorException e) {
-            return e.getResponseBodyAsString();
-        }
-
+    try {
+      return this.restTemplate.postForObject(
+          hermesUrl + "/rules/addRuleString", requestEntity, String.class);
+    } catch (HttpClientErrorException e) {
+      return e.getResponseBodyAsString();
     }
+  }
 
-    public String addRuleString(String ruleString) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        HttpEntity<String> requestEntity = new HttpEntity<>(ruleString, headers);
-
-        try {
-            return this.restTemplate.postForObject(hermesUrl + "/rules/addRuleString", requestEntity, String.class);
-        } catch (HttpClientErrorException e) {
-            return e.getResponseBodyAsString();
-        }
-
+  public Rule getRuleObject(long id) {
+    Rule rule = restTemplate.getForObject(hermesUrl + "/rules/getRuleObject/{id}", Rule.class, id);
+    if (rule == null) {
+      // TODO -> some error or sth... idk....
     }
+    return rule;
+  }
 
-    public Rule getRuleObject(long id) {
-        Rule rule = restTemplate.getForObject(hermesUrl + "/rules/getRuleObject/{id}", Rule.class, id);
-        if(rule == null) {
-            //TODO -> some error or sth... idk....
-        }
-        return rule;
+  public String getRuleString(long id) {
+    String ruleString =
+        restTemplate.getForObject(hermesUrl + "/rules/getRuleString/{id}", String.class, id);
+    if (ruleString == null || ruleString.isEmpty()) {
+      // TODO -> some error or sth... idk....
     }
-
-    public String getRuleString(long id) {
-        String ruleString = restTemplate.getForObject(hermesUrl + "/rules/getRuleString/{id}", String.class, id);
-        if(ruleString == null || ruleString.isEmpty()) {
-            //TODO -> some error or sth... idk....
-        }
-        return ruleString;
-    }
+    return ruleString;
+  }
 }
-
