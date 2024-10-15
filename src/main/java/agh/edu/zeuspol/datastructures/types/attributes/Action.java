e04b4b5004082df07d0b7c9351b146fc24ee95eb
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2024-10-15 10:51:14 +0200

    Action.request gets Params object

--- src/main/java/agh/edu/zeuspol/datastructures/types/attributes/Action.java
+++ src/main/java/agh/edu/zeuspol/datastructures/types/attributes/Action.java
@@ -1,30 +1,29 @@
 package agh.edu.zeuspol.datastructures.types.attributes;
 
-import java.util.Map;
 
 public enum Action {
 
     DEFAULT {
         @Override
-        public void request(Map<String, String> params) {
+        public void request(Params params) {
             System.out.println("Default action - requested");
         }
     },
     ACTION_1 {
         @Override
-        public void request(Map<String, String> params) {
+        public void request(Params params) {
             //TODO
             System.out.println("Action 1 - requested");
         }
     },
     ACTION_2 {
         @Override
-        public void request(Map<String, String> params) {
+        public void request(Params params) {
             //TODO
             System.out.println("Action 2 - requested");
         }
     },
     ;
 
-    public abstract void request(Map<String, String> params);
+    public abstract void request(Params params);
 }
